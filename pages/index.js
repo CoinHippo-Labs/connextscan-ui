3e51143508663ee0c653fe7105787a60bf00f889
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-05-05 15:58:50 +0700

    check ssr

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
 
 import { is_route_exist } from '../lib/routes'
 
@@ -7,15 +8,16 @@ export default () => {
   const { pathname, asPath } = { ...router }
   const _asPath = asPath.includes('?') ? asPath.substring(0, asPath.indexOf('?')) : asPath
 
+  const [ssr, setSsr] = useState(true)
+
+  useEffect(() => {
+    setSsr(false)
+  }, [])
+
   if (typeof window !== 'undefined' && pathname !== _asPath) {
     router.push(is_route_exist(_asPath) ? asPath : '/')
   }
-  if (typeof window === 'undefined' || pathname !== _asPath) {
-    return (
-      <div className="min-h-screen" />
-    )
-  }
-  return (
-    <div />
+  return !ssr && (
+    <Bridge />
   )
 }
