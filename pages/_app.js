51a4c0163f8c91de44f7b30383e726399e8c8019
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-10-12 23:01:24 +0700

    add ga

--- pages/_app.js
+++ pages/_app.js
@@ -99,6 +99,30 @@ export default ({
           name="theme-color"
           content="#050707"
         />
+        {
+          process.env.NEXT_PUBLIC_GA_TRACKING_ID &&
+          (
+            <>
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+                <script
+                  async
+                  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+                />
+                <script
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+                        page_path: window.location.pathname,
+                      });
+                    `,
+                  }}
+                />
+            </>
+          )
+        }
       </Head>
       <Provider store={store}>
         <Layout>
