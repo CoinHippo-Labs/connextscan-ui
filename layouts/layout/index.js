6791a444b7b3aa124d86529c1b3bafda58a51404
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-11-02 11:52:43 +0700

    styling

--- layouts/layout/index.js
+++ layouts/layout/index.js
@@ -160,7 +160,7 @@ export default ({
               minHeight: 'calc(100vh - 44px)',
               backgroundColor: theme === 'light' ?
                 '#f5f5f5' :
-                '#111011',
+                '#121111',
             }}
           >
             <Navbar />
