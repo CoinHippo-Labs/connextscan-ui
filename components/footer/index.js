18d9e3625f2c03bea4444f612715cfb25a1084af
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-11-03 00:02:36 +0700

    change footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,7 +1,8 @@
-import Link from 'next/link'
+// import Link from 'next/link'
 import { useSelector, shallowEqual } from 'react-redux'
 import moment from 'moment'
-import { FaHeart } from 'react-icons/fa'
+import { FaHeart, FaDiscord } from 'react-icons/fa'
+import { BsTwitter, BsTelegram, BsGithub } from 'react-icons/bs'
 
 import Image from '../image'
 import _package from '../../package.json'
@@ -9,12 +10,12 @@ import _package from '../../package.json'
 export default () => {
   const {
     preferences,
-    chains,
+    // chains,
   } = useSelector(state =>
     (
       {
         preferences: state.preferences,
-        chains: state.chains,
+        // chains: state.chains,
       }
     ),
     shallowEqual,
@@ -22,9 +23,9 @@ export default () => {
   const {
     theme,
   } = { ...preferences }
-  const {
-    chains_data,
-  } = { ...chains }
+  // const {
+  //   chains_data,
+  // } = { ...chains }
 
   const {
     dependencies,
@@ -32,7 +33,7 @@ export default () => {
 
   return (
     <div className={`${theme} footer flex flex-col md:flex-row items-center space-y-2.5 sm:space-y-0 p-3`}>
-      <div className="w-full md:w-1/2 lg:w-1/4 min-w-max flex items-center justify-center md:justify-start space-x-2">
+      <div className="w-full md:w-1/2 lg:w-1/3 min-w-max flex items-center justify-center md:justify-start space-x-2">
         <span>
           Built with
         </span>
@@ -76,7 +77,7 @@ export default () => {
           </a>
         )}
       </div>
-      <div className="hidden lg:flex w-full lg:w-2/4 flex-wrap items-center justify-center">
+      {/*<div className="hidden lg:flex w-full lg:w-2/4 flex-wrap items-center justify-center">
         {(chains_data || [])
           .map((c, i) => {
             const {
@@ -102,8 +103,84 @@ export default () => {
             )
           })
         }
+      </div>*/}
+      <div className="hidden lg:flex w-full lg:w-1/3 flex-wrap items-center justify-center space-x-2">
+        {process.env.NEXT_PUBLIC_TWITTER_USERNAME && (
+          <a
+            href={`https://twitter.com/${process.env.NEXT_PUBLIC_TWITTER_USERNAME}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsTwitter
+              size={20}
+              className="text-blue-400 dark:text-white"
+            />
+          </a>
+        )}
+        {process.env.NEXT_PUBLIC_TELEGRAM_USERNAME && (
+          <a
+            href={`https://t.me/${process.env.NEXT_PUBLIC_TELEGRAM_USERNAME}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsTelegram
+              size={20}
+              className="text-blue-500 dark:text-white"
+            />
+          </a>
+        )}
+        {process.env.NEXT_PUBLIC_DISCORD_URL && (
+          <a
+            href={process.env.NEXT_PUBLIC_DISCORD_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaDiscord
+              size={20}
+              className="text-blue-600 dark:text-white"
+            />
+          </a>
+        )}
+        {process.env.NEXT_PUBLIC_GITHUB_URL && (
+          <a
+            href={process.env.NEXT_PUBLIC_GITHUB_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsGithub
+              size={20}
+              className="text-black dark:text-white"
+            />
+          </a>
+        )}
+        {process.env.NEXT_PUBLIC_ENS_NAME && (
+          <a
+            href={`https://app.ens.domains/name/${process.env.NEXT_PUBLIC_ENS_NAME}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="min-w-max">
+              <div className="flex dark:hidden items-center">
+                <Image
+                  src="/logos/externals/ens/logo.png"
+                  alt=""
+                  width={20}
+                  height={20}
+                />
+              </div>
+              <div className="hidden dark:flex items-center">
+                <Image
+                  src="/logos/externals/ens/logo_white.png"
+                  alt=""
+                  width={20}
+                  height={20}
+                />
+              </div>
+            </div>
+          </a>
+        )}
       </div>
-      <div className="w-full md:w-1/2 lg:w-1/4 min-w-max flex items-center justify-center md:justify-end text-slate-400 dark:text-white space-x-1">
+      <div className="w-full md:w-1/2 lg:w-1/3 min-w-max flex items-center justify-center md:justify-end text-slate-400 dark:text-white space-x-1">
         <span>
           © {moment().format('YYYY')} made with
         </span>
