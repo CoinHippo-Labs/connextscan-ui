ccc6c410546dc04f8f976d837f2be599b47a87d9
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2023-01-27 02:37:50 +0700

    feat: remove image optimizer url

--- components/image/index.js
+++ components/image/index.js
@@ -1,23 +1,35 @@
 import Image from 'next/image'
 
-const loader = ({
-  src,
-  width,
-  quality,
-}) =>
-  `${process.env.NEXT_PUBLIC_IMAGE_OPTIMIZER_URL}/_next${
+const loader = (
+  {
+    src,
+    width,
+    quality = 75,
+  },
+) =>
+  `${
+    process.env.NEXT_PUBLIC_IMAGE_OPTIMIZER_URL ?
+      `${process.env.NEXT_PUBLIC_IMAGE_OPTIMIZER_URL}/_next` :
+      ''
+  }${
     src?.startsWith('/') ?
       '' :
       '/'
-  }${src}?url=${
-    src?.startsWith('/') ?
-      process.env.NEXT_PUBLIC_SITE_URL :
+  }${src}${
+    process.env.NEXT_PUBLIC_IMAGE_OPTIMIZER_URL ?
+      `?url=${
+        src?.startsWith('/') ?
+          process.env.NEXT_PUBLIC_SITE_URL :
+          ''
+      }${src}&w=${width}&q=${quality}` :
       ''
-  }${src}&w=${width}&q=${quality || 75}`
+  }`
 
-export default ({
-  ...rest
-}) => {
+export default (
+  {
+    ...rest
+  }
+) => {
   return (
     <Image
       { ...rest }
