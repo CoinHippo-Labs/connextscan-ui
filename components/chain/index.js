822a7a8cb913c39b36e558450a3f5b44814d0177
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2023-06-06 00:18:25 +0700

    feat: add chain's routers

--- components/chain/index.js
+++ components/chain/index.js
@@ -5,6 +5,7 @@ import _ from 'lodash'
 import moment from 'moment'
 import { utils } from 'ethers'
 
+import Routers from './routers'
 import Metrics from '../metrics'
 import SelectTimeframe from '../select-options/timeframe'
 import Volume from '../dashboard/volume'
@@ -24,6 +25,7 @@ export default () => {
     preferences,
     chains,
     assets,
+    router_asset_balances,
     pools,
     dev,
   } = useSelector(
@@ -32,35 +34,23 @@ export default () => {
         preferences: state.preferences,
         chains: state.chains,
         assets: state.assets,
+        router_asset_balances: state.router_asset_balances,
         pools: state.pools,
         dev: state.dev,
       }
     ),
     shallowEqual,
   )
-  const {
-    page_visible,
-  } = { ...preferences }
-  const {
-    chains_data,
-  } = { ...chains }
-  const {
-    assets_data,
-  } = { ...assets }
-  const {
-    pools_data,
-  } = { ...pools }
-  const {
-    sdk,
-  } = { ...dev }
+  const { page_visible } = { ...preferences }
+  const { chains_data } = { ...chains }
+  const { assets_data } = { ...assets }
+  const { router_asset_balances_data } = { ...router_asset_balances }
+  const { pools_data } = { ...pools }
+  const { sdk } = { ...dev }
 
   const router = useRouter()
-  const {
-    query,
-  } = { ...router }
-  const {
-    chain,
-  } = { ...query }
+  const { query } = { ...router }
+  const { chain } = { ...query }
 
   const [liquidity, setLiquidity] = useState(null)
   const [timeframe, setTimeframe] = useState(timeframes[1].day)
@@ -71,50 +61,32 @@ export default () => {
       const getData = async () => {
         if (page_visible && chain && sdk && chains_data && assets_data) {
           const response = toArray(await sdk.sdkUtils.getRoutersData())
-
-          const chain_data = getChain(chain, chains_data)
-
-          const {
-            chain_id,
-            domain_id,
-          } = { ...chain_data }
+          const { chain_id, domain_id } = { ...getChain(chain, chains_data) }
 
           const data =
             response
               .filter(d => d?.domain === domain_id)
               .map(d => {
-                const {
-                  local,
-                  balance,
-                } = { ...d }
+                const { local, balance } = { ...d }
 
                 let asset_data = getAsset(null, assets_data, chain_id, local)
-
                 asset_data = {
                   ...asset_data,
                   ...getContract(chain_id, asset_data?.contracts),
                 }
-
                 if (asset_data.contracts) {
                   delete asset_data.contracts
                 }
-
                 if (asset_data.next_asset && equalsIgnoreCase(asset_data.next_asset.contract_address, local)) {
                   asset_data = {
                     ...asset_data,
                     ...asset_data.next_asset,
                   }
-
                   delete asset_data.next_asset
                 }
 
-                const {
-                  decimals,
-                  price,
-                } = { ...asset_data }
-
+                const { decimals, price } = { ...asset_data }
                 const amount = Number(utils.formatUnits(BigInt(balance || '0'), decimals || 18))
-
                 return {
                   ...d,
                   chain_id,
@@ -129,7 +101,6 @@ export default () => {
       }
 
       getData()
-
       const interval = setInterval(() => getData(), 0.5 * 60 * 1000)
       return () => clearInterval(interval)
     },
@@ -145,14 +116,8 @@ export default () => {
           }
 
           const _timeframe = timeframes.find(t => t?.day === timeframe)
-          const chain_data = getChain(chain, chains_data)
-
-          const {
-            domain_id,
-          } = { ...chain_data }
-
+          const { domain_id } = { ...getChain(chain, chains_data) }
           const filters = { destination_chain: `eq.${domain_id}` }
-
           if (_timeframe?.day) {
             filters.transfer_date = `gt.${moment().subtract(_timeframe.day, 'days').startOf('day').format('YYYY-MM-DD')}`
           }
@@ -161,45 +126,28 @@ export default () => {
             toArray(await daily_transfer_volume(filters))
               .filter(v => v.transfer_date)
               .map(v => {
-                const {
-                  transfer_date,
-                  origin_chain,
-                  destination_chain,
-                  asset,
-                  volume,
-                  usd_volume,
-                } = { ...v }
-
+                const { transfer_date, origin_chain, destination_chain, asset, volume, usd_volume } = { ...v }
                 const origin_chain_data = getChain(origin_chain, chains_data)
                 const destination_chain_data = getChain(destination_chain, chains_data)
 
                 let asset_data = getAsset(null, assets_data, origin_chain_data?.chain_id, asset)
-
                 asset_data = {
                   ...asset_data,
                   ...getContract(origin_chain_data?.chain_id, asset_data?.contracts),
                 }
-
                 if (asset_data.contracts) {
                   delete asset_data.contracts
                 }
-
                 if (asset_data.next_asset && equalsIgnoreCase(asset_data.next_asset.contract_address, asset)) {
                   asset_data = {
                     ...asset_data,
                     ...asset_data.next_asset,
                   }
-
                   delete asset_data.next_asset
                 }
 
-                const {
-                  decimals,
-                  price,
-                } = { ...asset_data }
-
+                const { decimals, price } = { ...asset_data }
                 const amount = Number(utils.formatUnits(BigInt(volume || '0'), decimals || 18))
-
                 return {
                   ...v,
                   timestamp: moment(transfer_date, 'YYYY-MM-DD').startOf(_timeframe?.timeframe).valueOf(),
@@ -215,12 +163,7 @@ export default () => {
             toArray(await daily_transfer_metrics(filters))
               .filter(t => t.transfer_date)
               .map(t => {
-                const {
-                  transfer_date,
-                  origin_chain,
-                  destination_chain,
-                } = { ...t }
-
+                const { transfer_date, origin_chain, destination_chain } = { ...t }
                 const origin_chain_data = getChain(origin_chain, chains_data)
                 const destination_chain_data = getChain(destination_chain, chains_data)
 
@@ -232,45 +175,36 @@ export default () => {
                 }
               })
 
-          setData(
-            {
-              total_volume: _.sumBy(volumes, 'volume'),
-              volumes:
-                _.slice(
-                  _.orderBy(
-                    Object.entries(_.groupBy(volumes, 'timestamp'))
-                      .map(([k, v]) => {
-                        return {
-                          timestamp: Number(k),
-                          volume: _.sumBy(v, 'volume'),
-                        }
-                      }),
-                    ['timestamp'],
-                    ['asc'],
-                  ),
-                  -(timeframe || 52),
-                ),
-              total_transfers: _.sumBy(transfers, 'transfer_count'),
-              transfers:
-                _.slice(
-                  _.orderBy(
-                    Object.entries(_.groupBy(transfers, 'timestamp'))
-                      .map(([k, v]) => {
-                        return {
-                          timestamp: Number(k),
-                          transfers: _.sumBy(v, 'transfer_count'),
-                        }
-                      }),
-                    ['timestamp'],
-                    ['asc'],
-                  ),
-                  -(timeframe || 52),
-                ),
-            },
-          )
+          setData({
+            total_volume: _.sumBy(volumes, 'volume'),
+            volumes: _.slice(
+              _.orderBy(
+                Object.entries(_.groupBy(volumes, 'timestamp')).map(([k, v]) => {
+                  return {
+                    timestamp: Number(k),
+                    volume: _.sumBy(v, 'volume'),
+                  }
+                }),
+                ['timestamp'], ['asc'],
+              ),
+              -(timeframe || 52),
+            ),
+            total_transfers: _.sumBy(transfers, 'transfer_count'),
+            transfers: _.slice(
+              _.orderBy(
+                Object.entries(_.groupBy(transfers, 'timestamp')).map(([k, v]) => {
+                  return {
+                    timestamp: Number(k),
+                    transfers: _.sumBy(v, 'transfer_count'),
+                  }
+                }),
+                ['timestamp'], ['asc'],
+              ),
+              -(timeframe || 52),
+            ),
+          })
         }
       }
-
       getData()
     },
     [page_visible, chain, sdk, timeframe, chains_data, assets_data],
@@ -284,60 +218,52 @@ export default () => {
     // fees,
   } = { ...data }
 
-  const metrics =
-    liquidity && pools_data && data &&
-    {
-      liquidity: _.sumBy(liquidity, 'value') + (pools_data ? _.sumBy(pools_data.filter(p => p.chain_id === getChain(chain, chains_data)?.chain_id), 'tvl') : 0),
-      volume: total_volume,
-      transfers: total_transfers,
-      // fee: 33.33,
-    }
+  const metrics = liquidity && pools_data && data && {
+    liquidity: _.sumBy(liquidity, 'value') + (pools_data ? _.sumBy(pools_data.filter(p => p.chain_id === getChain(chain, chains_data)?.chain_id), 'tvl') : 0),
+    volume: total_volume,
+    transfers: total_transfers,
+    // fee: 33.33,
+  }
+
+  const { chain_id } = { ...getChain(chain, chains_data) }
+  const routers = router_asset_balances_data && _.orderBy(
+    Object.entries(_.groupBy(toArray(router_asset_balances_data[chain_id]), 'router_address')).map(([k, v]) => {
+      return {
+        router_address: k,
+        assets: _.orderBy(v, ['value'], ['desc']),
+        total_value: _.sumBy(v, 'value'),
+      }
+    }),
+    ['total_value'], ['desc'],
+  ).map((d, i) => { return { ...d, i } })
 
   return (
     <>
       <div className="mb-6">
-        <Metrics
-          data={metrics}
-          num_stats_days={timeframe}
-        />
+        <Metrics data={metrics} num_stats_days={timeframe} />
       </div>
       <div className="grid lg:grid-cols-4 gap-4 mt-2 mb-6 mx-auto">
         <div className="lg:col-span-4 flex items-center justify-end">
-          <SelectTimeframe
-            value={timeframe}
-            onSelect={t => setTimeframe(t)}
-          />
+          <SelectTimeframe value={timeframe} onSelect={t => setTimeframe(t)} />
         </div>
         <div className="lg:col-span-4">
-          <Volume
-            timeframe={timeframe}
-            volumes={volumes}
-          />
+          <Volume timeframe={timeframe} volumes={volumes} />
         </div>
         <div className="lg:col-span-4">
-          <Transfers
-            timeframe={timeframe}
-            transfers={transfers}
-          />
+          <Transfers timeframe={timeframe} transfers={transfers} />
         </div>
         {/*
           <div className="lg:col-span-4">
-            <Fee
-              timeframe={timeframe}
-              fees={fees}
-            />
+            <Fee timeframe={timeframe} fees={fees} />
           </div>
         */}
       </div>
-      <div className="flex items-start justify-between space-x-2">
-        <div className="w-full grid grid-flow-row lg:grid-cols-2 gap-4 mb-4">
-          <Assets
-            data={liquidity}
-          />
-          <Pools
-            data={pools_data}
-          />
-        </div>
+      <div className="w-full grid grid-flow-row lg:grid-cols-2 gap-4 mb-6">
+        <Assets data={liquidity} />
+        <Pools data={pools_data} />
+      </div>
+      <div className="w-full mb-4">
+        <Routers data={routers} />
       </div>
     </>
   )
---
+++ components/chain/routers.js
@@ -0,0 +1,214 @@
+import Link from 'next/link'
+import { useSelector, shallowEqual } from 'react-redux'
+import _ from 'lodash'
+import { TailSpin } from 'react-loader-spinner'
+
+import Copy from '../copy'
+import Datatable from '../datatable'
+import DecimalsFormat from '../decimals-format'
+import EnsProfile from '../ens-profile'
+import Image from '../image'
+import { ProgressBar } from '../progress-bars'
+
+import { currency_symbol } from '../../lib/object/currency'
+import { toArray, ellipse, loaderColor } from '../../lib/utils'
+
+export default ({ data }) => {
+  const { preferences } = useSelector(state => ({ preferences: state.preferences }), shallowEqual)
+  const { theme } = { ...preferences }
+
+  return (
+    <div className="space-y-4">
+      <div className="sm:flex sm:items-center sm:justify-between">
+        <div className="whitespace-nowrap uppercase text-sm font-semibold">
+          Routers
+        </div>
+      </div>
+      {data ?
+        <Datatable
+          columns={[
+            {
+              Header: '#',
+              accessor: 'i',
+              sortType: (a, b) => a.original.i > b.original.i ? 1 : -1,
+              Cell: props => (
+                <span className="font-semibold">
+                  {(props.flatRows?.indexOf(props.row) > -1 ? props.flatRows.indexOf(props.row) : props.value) + 1}
+                </span>
+              ),
+            },
+            {
+              Header: 'Address',
+              accessor: 'router_address',
+              disableSortBy: true,
+              Cell: props => {
+                const { value } = { ...props }
+                return value && (
+                  <div className="flex items-center space-x-1">
+                    <Link href={`/router/${value}`}>
+                      <EnsProfile
+                        address={value}
+                        noCopy={true}
+                        noImage={true}
+                        fallback={
+                          <span className="text-slate-400 dark:text-slate-200 text-sm font-semibold">
+                            <span className="xl:hidden">
+                              {ellipse(value, 8)}
+                            </span>
+                            <span className="hidden xl:block">
+                              {ellipse(value, 12)}
+                            </span>
+                          </span>
+                        }
+                      />
+                    </Link>
+                    <Copy value={value} />
+                  </div>
+                )
+              },
+            },
+            {
+              Header: 'By Asset',
+              accessor: 'assets',
+              sortType: (a, b) => a.original.total_value > b.original.total_value ? 1 : -1,
+              Cell: props => {
+                const { value } = { ...props }
+                return (
+                  <div className="grid grid-cols-2 gap-2">
+                    {toArray(value).map((v, i) => {
+                      const { asset_data, amount, value } = { ...v }
+                      const { symbol, image } = { ...asset_data }
+                      return (
+                        <div key={i} className="flex items-start space-x-2 mt-0.5">
+                          <Image
+                            src={image}
+                            width={18}
+                            height={18}
+                            className="rounded-full"
+                          />
+                          <div className="flex flex-col space-y-0.5">
+                            <span className="text-xs font-semibold">
+                              {symbol}
+                            </span>
+                            <div className="flex flex-col">
+                              <div className="h-4 flex items-center space-x-1">
+                                <span className="text-slate-400 dark:text-slate-500 text-2xs font-medium">
+                                  Amount:
+                                </span>
+                                <DecimalsFormat
+                                  value={amount}
+                                  noTooltip={true}
+                                  className="uppercase text-2xs font-semibold"
+                                />
+                              </div>
+                              <div className="h-4 flex items-center space-x-1">
+                                <span className="text-slate-400 dark:text-slate-500 text-2xs font-medium">
+                                  Liquidity:
+                                </span>
+                                <DecimalsFormat
+                                  value={value}
+                                  prefix={currency_symbol}
+                                  noTooltip={true}
+                                  className="uppercase text-2xs font-semibold"
+                                />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      )
+                    })}
+                  </div>
+                )
+              },
+              headerClassName: 'whitespace-nowrap justify-start',
+            },
+            {
+              Header: 'Liquidity',
+              accessor: 'total_value',
+              sortType: (a, b) => a.original.total_value > b.original.total_value ? 1 : -1,
+              Cell: props => {
+                const { value } = { ...props }
+                return (
+                  <div className="text-base font-bold text-right">
+                    {typeof value === 'number' ?
+                      <DecimalsFormat
+                        value={value}
+                        prefix={currency_symbol}
+                        noTooltip={true}
+                        className="uppercase"
+                      /> :
+                      <span className="text-slate-400 dark:text-slate-500">-</span>
+                    }
+                  </div>
+                )
+              },
+              headerClassName: 'whitespace-nowrap justify-end text-right',
+            },
+            {
+              Header: 'Relative Share %',
+              accessor: 'share',
+              sortType: (a, b) => a.original.total_value > b.original.total_value ? 1 : -1,
+              Cell: props => {
+                const { flatRows, row } = { ...props }
+                const index = flatRows?.indexOf(row)
+                const total = _.sumBy(data, 'total_value')
+
+                const _data =
+                  index > -1 ?
+                    _.slice(
+                      flatRows.map(d => {
+                        const { original } = { ...d }
+                        const { total_value } = { ...original }
+                        return {
+                          ...original,
+                          value_share: total_value * 100 / total,
+                        }
+                      }),
+                      0,
+                      index + 1,
+                    ) :
+                    []
+
+                const { value_share } = { ..._.last(_data) }
+                const total_share = value_share // _.sumBy(_data, 'value_share')
+                return (
+                  <div className="flex items-start space-x-1.5 mt-0.5">
+                    <div className="w-20 bg-zinc-200 dark:bg-zinc-800 mt-0.5">
+                      <div style={{ width: `${total_share}%` }}>
+                        <ProgressBar
+                          width={(total_share - value_share) * 100 / total_share}
+                          color="bg-blue-200"
+                          backgroundClassName="h-7 bg-blue-500"
+                          className="h-7"
+                        />
+                      </div>
+                    </div>
+                    <DecimalsFormat
+                      value={total_share}
+                      format="0,0.0"
+                      suffix="%"
+                      noTooltip={true}
+                      className="text-slate-600 dark:text-slate-200 text-2xs font-medium"
+                    />
+                  </div>
+                )
+              },
+              headerClassName: 'whitespace-nowrap justify-start',
+            },
+          ]}
+          data={data}
+          noPagination={data.length <= 10}
+          defaultPageSize={50}
+          className="no-border"
+        /> :
+        <div className="flex items-center my-3">
+          <TailSpin
+            width="32"
+            height="32"
+            color={loaderColor(theme)}
+          />
+        </div>
+      }
+    </div>
+  )
+}
