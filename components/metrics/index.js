6751fdf3ca9f97a8ea837b3030dbfb144ae4c8d0
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2023-12-12 17:12:42 +0700

    chore: add transfers metric

--- components/metrics/index.js
+++ components/metrics/index.js
@@ -17,13 +17,13 @@ export default ({ data, numDays = NUM_STATS_DAYS }) => {
   const { query } = { ...router }
   const { chain } = { ...query }
 
-  const { liquidity, volume, supported_chains } = { ...data }
+  const { liquidity, volume, transfers, supported_chains } = { ...data }
   const metricClassName = 'bg-slate-50 dark:bg-slate-900 dark:bg-opacity-75 border dark:border-slate-900 rounded-lg space-y-0.5 py-3 px-4'
   const titleClassName = 'text-slate-400 dark:text-slate-200 text-base font-medium'
   const valueClassName = 'text-3xl font-bold'
 
   return (
-    <div className={`w-full grid grid-flow-row sm:grid-cols-2 lg:grid-cols-${2 + (chain ? 0 : 1)} gap-5`}>
+    <div className={`w-full grid grid-flow-row sm:grid-cols-2 lg:grid-cols-${2 + (transfers ? 1 : 0) + (chain ? 0 : 1)} gap-5`}>
       <div className={metricClassName}>
         <span className={titleClassName}>
           Liquidity
@@ -58,6 +58,19 @@ export default ({ data, numDays = NUM_STATS_DAYS }) => {
           }
         </div>
       </div>
+      {transfers > 0 && (
+        <div className={metricClassName}>
+          <span className={titleClassName}>
+            Transfers
+          </span>
+          <div>
+            {data ?
+              <NumberDisplay value={transfers} className={valueClassName} /> :
+              <Spinner width={32} height={32} />
+            }
+          </div>
+        </div>
+      )}
       {!chain && (
         <div className={metricClassName}>
           <span className={titleClassName}>
